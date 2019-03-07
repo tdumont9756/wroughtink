d7e3681bc204dc9f20997b2639d6bde701109c16
Travis Dumont <tdumont97566@gmail.com>
2019-03-07 05:57:48 -0600

    added image discription to lightbox instead of dummy text

--- front-page.php
+++ front-page.php
@@ -27,7 +27,11 @@ function wi_folklore_markup(){
 
   // 2) filter the gallery out of home page:
   $blocks = parse_blocks($home_page->post_content);
+	// echo print_r($blocks);
 	$image_ids = $blocks[0]['attrs']['ids'];
+
+
+	echo $attachments->post_content;
 
 	//* 3) for each image in the gallery add extra html markup
   //* 4) create the template for the light box
@@ -35,13 +39,14 @@ function wi_folklore_markup(){
 	$lightbox = "";
 	$counter = 1;
 	foreach($image_ids as $image){
+		$discription = get_post($image)->post_content;
 		$display .= "<article class='wi-gallery-img' data-featherlight='#fl".(string) $counter ."'>";
 		$display .=  wp_get_attachment_image($image);
 		$display .= "</article>";
 
 		$lightbox .= "<div id='fl".(string) $counter ."' class='lightbox'>";
 		$lightbox .= wp_get_attachment_image($image);
-		$lightbox .= "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>";
+		$lightbox .= "<p>". $discription ."</p>";
 		$lightbox .= " <div class='lightbox-btn-container'> ";
 		$lightbox .= " <button>View Lore</button> ";
 		$lightbox .= " <button>View Product</button> ";
